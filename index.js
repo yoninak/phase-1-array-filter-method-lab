7e88dd37ac70c5039bca72f9115494cd170a81dc
Yoninia Karmely <yoninaweinberg28@gmail.com>
2023-02-22 10:33:27 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(array, string){
+    const result = array.filter(word => word.toLowerCase()=== string.toLowerCase());
+  return result
+    
+}
+
+function fuzzyMatch(array, string){
+    const result = array.filter(letters => letters.charAt(0) === string.charAt(0));
+  return result
+}
+
+function matchName(array, string){
+    const result = array.filter(object => object.name === string);
+  return result
+}
